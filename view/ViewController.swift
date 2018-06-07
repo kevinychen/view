1081a3087dc8077bd337794ef5217b5ea70c042a
Kevin Y. Chen <kchen@palantir.com>
2018-06-06 22:07:39 -0700

    Initial Commit

---
+++ view/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  view
+//
+//  Created by Kevin Chen on 6/6/18.
+//  Copyright © 2018 Kevin Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
